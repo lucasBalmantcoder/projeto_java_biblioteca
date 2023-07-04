82f003d47b4dbce023d402d79a95fb177c831882
minato <lucaspatrickr5@gmail.com>
2023-07-04 09:14:47 -0300

    nova atualizacao 04/07/23

---
+++ universidade/BibliotecaUnivesidade.java
@@ -0,0 +1,11 @@
+package universidade;
+
+import java.io.File;
+
+public class BibliotecaUnivesidade {
+    
+    //Arquivo para salvar os dados
+
+    private final File file = new File("biblioteca.liv");
+}
+
